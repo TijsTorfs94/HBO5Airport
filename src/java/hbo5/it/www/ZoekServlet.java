b7933367f0fc96e2f6ba57d722da8efff832f107
IFunkUp <steve.dekerf@hotmail.com>
2017-05-02 09:37:10 +0200

    last commit

--- src/java/hbo5/it/www/ZoekServlet.java
+++ src/java/hbo5/it/www/ZoekServlet.java
@@ -72,12 +72,12 @@ public class ZoekServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            
-        } catch (Exception e) {
-        }
+        
+        
+        
+        
+        
+        
     }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
--- src/java/hbo5/it/www/dataaccess/DAPersoon.java
+++ src/java/hbo5/it/www/dataaccess/DAPersoon.java
@@ -5,26 +5,53 @@
  */
 package hbo5.it.www.dataaccess;
 
+import hbo5.it.www.ZoekServlet;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import org.jboss.logging.Logger;
 
 /**
  *
  * @author c1040604
  */
 public class DAPersoon {
-        private Connection connection = null;
+        private final Connection connection = null;
 
 public DAPersoon(String url, String login, String password, String driver)   throws ClassNotFoundException, SQLException {
         Class.forName(driver);
-        connection = DriverManager.getConnection(url, login, password);
+        conn = DriverManager.getConnection(url, login, password);
     }
     
     public void close() throws SQLException {
         if (connection != null) {
             connection.close();
         }  
+    }
+    
+    Connection conn;
+    CallableStatement SP = null;
+    
+    public Boolean CheckLogin(String name, String Pass){
+        
+        Boolean result = false;
+        try {
+            SP = conn.prepareCall("{call chklogin(?,?)}");
+            SP.setString("p_pers_login", name);
+            SP.setString("p_pers_pas", Pass);
+            result = SP.execute();
+        } catch (SQLException e) {
+          //  Logger.getLogger(ZoekServlet.class.getName()).log(Level.SEVERE,null);
+        }
+        
+        
+        
+        
+        
+        
+        return result;
     }
 
 }
